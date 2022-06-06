263963b86ac966646fe31e52d638b19397ec46df
Mark <labaclado.mj@gmail.com>
2022-06-07 04:18:27 +0800

    added format for achievements and activities

--- upv-website/data 2.py
+++ upv-website/data 2.py
@@ -1,22 +1,51 @@
 activities = {
-	"major": {
-        "category": "Major Events",
-		"activity":["Grand Outreach", "Grand Educational Discussion", "Statistical Seminar", "Grand Pakain"]
+	"Major Events": 
+    {
+        "Grand Outreach":
+        {
+            "desc":"",
+            "img_file":""
+        },
+
+        "Grand Educational Discussion":
+        {
+            "desc":"",
+            "img_file":""
+        },
+
+        "Statistical Seminar":
+        {
+            "desc":"",
+            "img_file":""
+        },
+
+        "Grand Pakain":
+        {
+            "desc":"",
+            "img_file":""
+        },
     },
 
-    "internal": {
-        "category": "Internal Org Activities",
+    "Internal Org Activities": 
+    {
+        "category": ,
 		"activity":["Kumustahan Session", "Educational Discussions for Socio-political awareness", "Bonding activities (Game & Movie Nights)", "Committee Wars", "Semstarter", "Sem-ender", "Pasafest"]
     },
 
-    "initiatives": {
-        "category": "Org Initiatives",
+    "Org Initiatives": {
 		"activity":["Academic Tutorials", "Statistical Challenge", "FST initiatives", "Thesis Statistical Assistance", "Career Orientation", "Project FAM"]
     }
 }
 
 
 achievements = {
+    "Organization with Great Social Contribution During Pandemic (Linggo ng Parangal 2021)": 
+    {
+        "caption":"",
+        "img_file":""
+    },
+
+
     "Achievements" : ["Organization with Great Social Contribution During Pandemic (Linggo ng Parangal 2021)", "Best Youth Group Advocate for Statistics (2019 to 2021)", "4-time Defending Overall Champion in Statistical Wars"]
 }
 
