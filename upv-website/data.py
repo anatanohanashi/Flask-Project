887066004ba1f3c4a156c6de094319a20c8dc45c
Mark <labaclado.mj@gmail.com>
2022-06-07 01:38:17 +0800

    Added to data.py

---
+++ upv-website/data.py
@@ -0,0 +1,13 @@
+committees = {
+	"arc": {
+		"name": "Academic Resource Committee"
+		"function": "To blah blah"
+		"members":["Member 1, Member 2, Member 3"]
+		
+	}
+}
+
+
+eb = {
+	"President" : "Gia"
+}
