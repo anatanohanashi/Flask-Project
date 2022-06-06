225c77d6a23440bc312e35ff72f7d33a006b94cc
Mark <labaclado.mj@gmail.com>
2022-06-07 00:38:27 +0800

    Added main pages (index, events, contacts, about, etc)

--- upv-website/app.py
+++ upv-website/app.py
@@ -4,13 +4,27 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return """Welcome to UP Variates!</h1>"""
+    return render_template("index.html")
 
 @app.route('/org_structure')
 def org_structure():
-    return """Welcome to UP Variates!</h1>"""
     return render_template('org_structure.html')
 
+@app.route('/events')
+def events():
+    return render_template('events.html')
+
+@app.route('/achievements')
+def achievements():
+    return render_template('achievements.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
