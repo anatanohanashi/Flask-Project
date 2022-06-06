490af328b6679aab40e2de622cb5d2faea0c5fec
Gia <gdabubo@up.edu.ph>
2022-06-07 03:27:44 +0800

    activities data 2.py

---
+++ upv-website/data 2.py
@@ -0,0 +1,16 @@
+activities = {
+	"major": {
+        "category": "Major Events",
+		"activity":["Grand Outreach", "Grand Educational Discussion", "Statistical Seminar", "Grand Pakain"]
+    },
+
+    "internal": {
+        "category": "Internal Org Activities",
+		"activity":["Kumustahan Session", "Educational Discussions for Socio-political awareness", "Bonding activities (Game & Movie Nights)", "Committee Wars", "Semstarter", "Sem-ender", "Pasafest"]
+    },
+
+    "initiatives": {
+        "category": "Org Initiatives",
+		"activity":["Academic Tutorials", "Statistical Challenge", "FST initiatives", "Thesis Statistical Assistance", "Career Orientation", "Project FAM"]
+    }
+}
