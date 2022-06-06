c4b9c9f10c5894300e3057ea8f5ec67991de0081
Gia <gdabubo@up.edu.ph>
2022-06-07 02:29:44 +0800

    comm and eb data.py

--- upv-website/data.py
+++ upv-website/data.py
@@ -2,12 +2,62 @@ committees = {
 	"arc": {
 		"name": "Academic Resource Committee"
 		"function": "To blah blah"
-		"members":["Member 1, Member 2, Member 3"]
-		
+        "chairperson": "Krishna Chelsea Garay"
+		"members":["Patricia Faye Anne Banzon", "Joan Catolico", "Airam Dave Cuyos", "Jester Vince De Torres", "Micah Halamani", "Holito Jr. Perez", "Maria Cecilia San Juan", "Paula Beatriz Santos", "James Orlan Sario"]
+	}
+
+    "exte": {
+		"name": "External Affairs Committee"
+		"function": "To blah blah"
+        "chairperson": "Lois Alfred Tolentino"
+		"members":["Jasmine Amor Avelino", "Christian Patrick Bitun", "Charissa May Sta. Maria", "Arnon Christien Cruz", "Meeka Angelu Gray", "Hazel Anne Paguirigan", "Luis Augusto Tan", "Carissa Elaine Varon"]
+	}
+
+    "fin": {
+		"name": "Finance Committee"
+		"function": "To blah blah"
+        "chairperson": "Jove Mutya Omamos"
+		"members":["James Inigo Alcantara", "Denilyn Joy Alcantara", "Comei Carpio", "Rennsy Myle Castillo", "Carina Sophia Miranda", "Cyrus Toring", "Vanessa Ann Trinidad", "Noreen Yan"]
+	}
+
+    "inte": {
+		"name": "Internal Affairs Committee"
+		"function": "To blah blah"
+        "chairperson": "Loren Angela Hernandez"
+		"members":["Alysa Maxine Dulay", "Lance Christian Gulay", "Keana Maala", "Eloisa Mae Obmina", "Jerwind Pineda", "Claire Joyce Prendol", "John Lloyd Sabusap", "Ann Rossell Tuyogon"]
+	}
+
+    "mem": {
+		"name": "Membership Committee"
+		"function": "To blah blah"
+        "chairperson": "Eanna Abbygale Yem"
+		"members":["Christeane Jannah Albina", "Julian Louise Castro", "Angelo Daquiz", "Josefa Millen Fusingan", "Nicole Louise Juguilon", "Alane Patricia Padillo", "Kimberly Jan Remolano", "Charles Reynard Valencia"]
+	}
+
+    "sec": {
+		"name": "Secretariat Committee"
+		"function": "To blah blah"
+        "chairperson": "Ian Eimerson Mercado"
+		"members":["Roger Jr. Antonio", "Czarina Apdon", "Ma. Czifriani De Mesa", "Jian Dequina", "Katherine Mae Gonzalvo", "Viktoria Cecilia Manipis", "Niel Jasper Pena", "Christia Tangin", "Lyzelle Anne Teruel"]
+	}
+
+    "scc": {
+		"name": "Social Consciousness Committee"
+		"function": "To blah blah"
+        "chairperson": "Noeliano Ocorro"
+		"members":["Christian Claudde Aure", "Wyomi Chloe Betis", "Diether Candelario", "Dominic Gamboa", "Marc Shervin Ignacio", "Sophia Palad", "Ray Albert Pujeda", "Aquilla Luke Villamor", "Jean Phoebe Yao"]
 	}
 }
 
 
 eb = {
-	"President" : "Gia"
+	"President" : ["Gia Mizrane Abubo", "images/eb/GIA.jpg"],
+    "Vice President" : ["Joshua Figuracion", "images/eb/JOSHUA.jpg"],
+    "Academic Resource Committee Chaiperson" : ["Krishna Chelsea Garay", "images/eb/KRISHNA.jpg"],
+    "External Affairs Committee Chairperon" : ["Lois Alfred Tolentiono", "images/eb/LOIS.jpg"],
+    "Finance Committee Chairperson" : ["Jove Mutya Omamos", "images/eb/JOVE.jpg"],
+    "Internal Affairs Committee Chairperson" : ["Loren Angela Hernandez", "images/eb/LOREN.jpg"],
+    "Membership Committee" : ["Eanna Abbygale Yem", "images/eb/EANNA.jpg"],
+    "Secretariat Committee" : ["Ian Eimerson Mercado", "images/eb/IAN.jpg"],
+    "Social Consciousness Committee" : ["Noeliano Ocorro", "images/eb/NOEL.jpg"]
 }
