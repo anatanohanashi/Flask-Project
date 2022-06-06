03320aa6c58f6dfa87edcf2a746b687fb0bac374
Gia <gdabubo@up.edu.ph>
2022-06-07 02:51:46 +0800

    functions 1 data.py

--- upv-website/data.py
+++ upv-website/data.py
@@ -1,45 +1,45 @@
 committees = {
 	"arc": {
         "name": "Academic Resource Committee",
-		"function": "To blah blah",
+		"function": "The Academic Resource Committee is in-charge of all academic and other educational matters of the organization. It promotes and maintains a high level of educational value among the members of the organization. Moreover, it provides the other communities with necessary researches and conducts studies on the ways and means for the utmost effectivity of the activities of the organization.",
         "chairperson": "Krishna Chelsea Garay",
 		"members":["Patricia Faye Anne Banzon", "Joan Catolico", "Airam Dave Cuyos", "Jester Vince De Torres", "Micah Halamani", "Holito Jr. Perez", "Maria Cecilia San Juan", "Paula Beatriz Santos", "James Orlan Sario"]
-    }
+    },
 
     "exte": {
 		"name": "External Affairs Committee"
-		"function": "To blah blah"
+		"function": "The External Affairs Committee represents and coordinates all projects, activities, and functions done in cooperation with other organizations. It also coordinates with the Secretariat Committee in fostering good relationship with other organizations, offices, or individuals and is in-charge of establishing contacts in and out the university. Furrthermore, it spearheada trainings and career opportunities for the members, and solicitations for the activities of the organization."
         "chairperson": "Lois Alfred Tolentino"
 		"members":["Jasmine Amor Avelino", "Christian Patrick Bitun", "Charissa May Sta. Maria", "Arnon Christien Cruz", "Meeka Angelu Gray", "Hazel Anne Paguirigan", "Luis Augusto Tan", "Carissa Elaine Varon"]
-	}
+	},
 
     "fin": {
 		"name": "Finance Committee",
-		"function": "To blah blah",
+		"function": "The Finance Committee plans and facilitates the execution of income generating programs of the organization. It reviews and recommends to the Executive Board the budgets and plans for the semestral finances of the organization. In addition, it submits a monthly financial report of the organization, and supervises all fund raising activities of the organization.",
         "chairperson": "Jove Mutya Omamos",
 		"members":["James Inigo Alcantara", "Denilyn Joy Alcantara", "Comei Carpio", "Rennsy Myle Castillo", "Carina Sophia Miranda", "Cyrus Toring", "Vanessa Ann Trinidad", "Noreen Yan"]
-	}
+	},
 
     "inte": {
 		"name": "Internal Affairs Committee",
-		"function": "To blah blah",
+		"function": "The internal Affairs Committee enforces all guidelines set for all internal activities of the organization including the responsibility of all logistical matters for events handled by the organization. It facilitates the improvement and maintenance of an orderly and  congenial “atmosphere” in the tambayan. Aside from this, it is also in-charge of all the facilities and properties of the organization. The committee notifies the members of meetings and other activities of the organization. Member of Internal Affairs Committee act as a property custodian and shall submit a semestral inventory of the properties of the organization.",
         "chairperson": "Loren Angela Hernandez",
 		"members":["Alysa Maxine Dulay", "Lance Christian Gulay", "Keana Maala", "Eloisa Mae Obmina", "Jerwind Pineda", "Claire Joyce Prendol", "John Lloyd Sabusap", "Ann Rossell Tuyogon"]
-	}
+	},
 
     "mem": {
 		"name": "Membership Committee",
-		"function": "To blah blah",
+		"function": "The Membership Committee looks after the welfare and the development of the members and the applicants of the organization. It formulates and implements the rules and guidelines for the application, probation, acceptance, and induction of the applicants and the renewals of the members. Moreover, the committee formulates, enforces, and administers rules and guidelines regarding the citations and disciplinary measures . It also recommends to the Executive Board the members who shall receive citations or disciplinary measures in accordance with the constitution. Furthermore, Membership Committee is in-charge of maintaining pertinent records of the members of the organization. ",
         "chairperson": "Eanna Abbygale Yem",
 		"members":["Christeane Jannah Albina", "Julian Louise Castro", "Angelo Daquiz", "Josefa Millen Fusingan", "Nicole Louise Juguilon", "Alane Patricia Padillo", "Kimberly Jan Remolano", "Charles Reynard Valencia"]
-	}
+	},
 
     "sec": {
 		"name": "Secretariat Committee",
 		"function": "To blah blah",
         "chairperson": "Ian Eimerson Mercado",
 		"members":["Roger Jr. Antonio", "Czarina Apdon", "Ma. Czifriani De Mesa", "Jian Dequina", "Katherine Mae Gonzalvo", "Viktoria Cecilia Manipis", "Niel Jasper Pena", "Christia Tangin", "Lyzelle Anne Teruel"]
-	}
+	},
 
     "scc": {
 		"name": "Social Consciousness Committee",
