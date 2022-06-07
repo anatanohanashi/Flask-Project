9f506ba0321b6c38c5ad804067cdc12bd81cc2d6
Mark <labaclado.mj@gmail.com>
2022-06-07 14:43:43 +0800

    Fixed images on achievements and events

--- upv-website/database.py
+++ upv-website/database.py
@@ -71,19 +71,73 @@ eb = {
 activities = {
 	"Major Events": 
     {
+        "Grand Educational Discussion":
+        {
+            "desc":"In realizing the members’ responsibility to serve as “Iskolar ng bayan”, UP Variates provides venues that promote awareness and responsiveness to university and social issues. It regularly holds educational discussions that present social issues in and out of the university.",
+            "img_file":"static/images/events/ED_1.jpg"
+        },
+
         "Grand Outreach":
         {
-            "desc":"Some description",
-            "img_file":"static/images/eb/EANNA.jpg"
-        }
-	}
+            "desc":"UP Variates yearly conducts a Grand Outreach in their chosen community or beneficiary. This enables the members to help in different ways such as for the environment through a clean up drive, tree planting, and street cleaning. It can also focus on giving support to the people such as having tutorials on out-of-school youth, outreach programs for the kids in the orphanage, and conducting medical check-ups for a community.",
+            "img_file":"static/images/events/Outreach_1.jpg"
+        },
+
+        "Donation Drive":
+        {
+            "desc":"The organization conducts donation drives to reach out and help the people in need. In the previous year, UP Variates initiated a donation drive for the people gravely affected by the pandemic such as the Frontliners and Jeepney drivers in the UP Community.",
+            "img_file":"static/images/events/Donation_Drive_1.jpg"
+        },
+    },
+
+    "Internal Org Activities": 
+    {
+        "Kumustahan Session":
+        {
+            "desc":"UP variates conducts Kamustahan Sessions to check the situation of its members, especially in this pandemic.",
+            "img_file":"static/images/events/Kumustahan_1.png"
+        },
+
+        "Bonding Activities":
+        {
+            "desc":"Bonding activities such as Committee Wars, Semstarter, Game and Movie Nights are also organized which enable the members to destress and have more interaction with each other.",
+            "img_file":" "
+        },
+    },
+
+    "Other Initiatives": 
+    {
+        "Academic Tutorials":
+        {
+            "desc": "UP Variates holds tutorials on Statistics and Mathematics courses for the students to prepare them for their upcoming long examinations.",
+            "img_file":""
+        },
+
+        "Statistical Challenge":
+        {
+            "desc": "The Statistical Challenge is a national statistical quiz bee among junior high schools. It aims to reach out to society by engaging Grade 8-10 students from all over the country to test their statistical wits and skills.",
+            "img_file":"static/images/events/Statcha_1.jpg"
+        },
+
+        "Statistical Chapter":
+        {
+            "desc": "As an alternative to the cancelled Statistical Challenge due to the limitations this pandemic, Statistical Chapters was launched last October 2020. This is a month-long initiative that aims to discuss various statistical topics that were covered by the annual friendly competition, Statistical Challenge. Its goal was to explain and elaborate Statistical concepts in simple terms that can be easily grasped by the readers. ",
+            "img_file":"static/images/events/Stat_Chap_1.png"
+        },
+    }
 }
 
 
 achievements = {
-    "Organization with Great Social Contribution During Pandemic (Linggo ng Parangal 2021)": 
+    "Organization with Great Social Contribution During Pandemic": 
     {
-        "caption":"Some caption",
-        "img_file":"static/images/eb/EANNA.jpg"
+        "caption":"Acknowledged by the OVCSA (Office of the Vice-Chancellor for Student Affairs) as one of the organizations who had great social contributions during the pandemic in the Linggo ng Parangal 2021",
+        "img_file":"static/images/awards/SOCIAL_CONTRIBUTION.jpg"
+    },
+
+    "Best Youth Group Advocate for Statistics": 
+    {
+        "caption":"Recognized as the Best Youth Group Advocate for Statistics by the Philippine Statistical System for three consecutive years for its “unparalleled performance and exemplary dedication towards the successful conduct of the statistical advocacy activities”",
+        "img_file":"static/images/awards/YOUTH_GROUP_ADVOCATE.jpg"
     }
 }
