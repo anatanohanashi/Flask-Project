989450a85a8938c08fe2c9e4aa79ad79a8ae7ae8
Gia <gdabubo@up.edu.ph>
2022-06-07 08:23:14 +0800

    pics data 2.py

--- upv-website/data 2.py
+++ upv-website/data 2.py
@@ -4,19 +4,19 @@ activities = {
         "Grand Educational Discussion":
         {
             "desc":"In realizing the members’ responsibility to serve as “Iskolar ng bayan”, UP Variates provides venues that promote awareness and responsiveness to university and social issues. It regularly holds educational discussions that present social issues in and out of the university.",
-            "img_file":""
+            "img_file":"static/images/events/ED 1.jpg"
         },
 
         "Grand Outreach":
         {
             "desc":"UP Variates yearly conducts a Grand Outreach in their chosen community or beneficiary. This enables the members to help in different ways such as for the environment through a clean up drive, tree planting, and street cleaning. It can also focus on giving support to the people such as having tutorials on out-of-school youth, outreach programs for the kids in the orphanage, and conducting medical check-ups for a community.",
-            "img_file":""
+            "img_file":"static/images/events/Outreach 1.jpg"
         },
 
         "Donation Drive":
         {
             "desc":"The organization conducts donation drives to reach out and help the people in need. In the previous year, UP Variates initiated a donation drive for the people gravely affected by the pandemic such as the Frontliners and Jeepney drivers in the UP Community.",
-            "img_file":""
+            "img_file":"static/images/events/Donation Drive 1.jpg"
         },
     },
 
@@ -25,13 +25,13 @@ activities = {
         "Kumustahan Session":
         {
             "desc":"UP variates conducts Kamustahan Sessions to check the situation of its members, especially in this pandemic.",
-            "img_file":""
+            "img_file":"static/images/events/Kumustahan 1.jpg"
         },
 
         "Bonding Activities":
         {
-            "desc":"Bonding activities such as Game and Movie Nights are also organized which enable the members to destress and have more interaction with each other.",
-            "img_file":""
+            "desc":"Bonding activities such as Committee Wars, Semstarter, Game and Movie Nights are also organized which enable the members to destress and have more interaction with each other.",
+            "img_file":" "
         },
     },
 
@@ -43,28 +43,16 @@ activities = {
             "img_file":""
         },
 
-        "Academic Tutorials":
+        "Statistical Challenge":
         {
             "desc": "The Statistical Challenge is a national statistical quiz bee among junior high schools. It aims to reach out to society by engaging Grade 8-10 students from all over the country to test their statistical wits and skills.",
-            "img_file":""
+            "img_file":"static/images/events/Statcha 1.jpg"
         },
 
         "Statistical Chapter":
         {
             "desc": "As an alternative to the cancelled Statistical Challenge due to the limitations this pandemic, Statistical Chapters was launched last October 2020. This is a month-long initiative that aims to discuss various statistical topics that were covered by the annual friendly competition, Statistical Challenge. Its goal was to explain and elaborate Statistical concepts in simple terms that can be easily grasped by the readers. ",
-            "img_file":""
-        },
-
-       "FST Initiatives":
-        {
-            "desc": "The organization actively conducts initiatives to help and guide the freshie, shiftee, and transferee students in UP School of Statistics.",
-            "img_file":""
-        },
-
-        "Project FAM":
-        {
-            "desc": "Project FAM is a joint initiative of Finance, Academic, and Membership Committee which aims to help the members who struggle due to pandemic and remote learning setup.",
-            "img_file":""
+            "img_file":"static/images/events/Stat Chap 1.jpg"
         },
     }
 }
